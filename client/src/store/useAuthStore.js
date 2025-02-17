15746d19bbd26a47b4eecb8cedee32ec8bdf8bb9
viswa15 <viswatejatummala@gmail.com>
2025-02-17 22:14:38 +0530

    last-commit

--- client/src/store/useAuthStore.js
+++ client/src/store/useAuthStore.js
@@ -4,7 +4,7 @@ import {axiosInstance} from "../lib/axios.js";
 import {toast} from "react-hot-toast"
 import {io} from "socket.io-client"
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001" : "/api"
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001" : "/"
 
 export const useAuthStore = create(
     devtools((set,get) => ({
