c76786147b4d619f47a2f077d4670b584d357fbc
viswa15 <viswatejatummala@gmail.com>
2025-08-10 19:50:11 +0530

    production-commit

--- server/src/index.js
+++ server/src/index.js
@@ -13,24 +13,21 @@ dotenv.config();
 
 const PORT = process.env.PORT;
 
-const __dirname = path.resolve();
+// const __dirname = path.resolve();
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(cookieParser());
-app.use(cors({
-    origin : "http://localhost:5173",
-    credentials : true
-}));
+app.use(cors());
 app.use("/api/auth",authRoute);
 app.use("/api/messages",messageRoute)
 
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname,"../client/dist")));
-    app.get("*",(req,res)=>{
-        res.sendFile(path.join(__dirname,"../client/dist/index.html"))
-    })
-}
+// if(process.env.NODE_ENV === "production"){
+//     app.use(express.static(path.join(__dirname,"../client/dist")));
+//     app.get("*",(req,res)=>{
+//         res.sendFile(path.join(__dirname,"../client/dist/index.html"))
+//     })
+// }
 
 server.listen(PORT,()=>{
     console.log("Server running on port:",PORT);
