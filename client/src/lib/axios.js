dee95d1e22c107e5f987350388bd69e89978f022
viswa15 <viswatejatummala@gmail.com>
2025-08-11 11:53:36 +0530

    client-production-1

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,6 +1,7 @@
 import axios  from "axios";
+const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 export const axiosInstance = axios.create({
-    baseURL : import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+    baseURL : `${BASE_URL}/api`,
     withCredentials : true
 })
