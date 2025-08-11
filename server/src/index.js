8bc55a762155872678d2369b62739f78fd2485ef
viswa15 <viswatejatummala@gmail.com>
2025-08-11 11:48:42 +0530

    adding-new-var

--- server/src/index.js
+++ server/src/index.js
@@ -18,7 +18,12 @@ const PORT = process.env.PORT;
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(cookieParser());
-app.use(cors());
+app.use(cors(
+    {
+        origin: process.env.VITE_CLIENT_URL || "http://localhost:5173",
+        credentials: true
+    }
+));
 app.use("/api/auth",authRoute);
 app.use("/api/messages",messageRoute)
 
